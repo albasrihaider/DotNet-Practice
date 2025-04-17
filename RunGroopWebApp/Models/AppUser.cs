ede0b97a0244dd038877fe09b50c483de6ef4f75
Haider Albasri <haiderf3@gmail.com>
2025-04-17 05:03:54 +0300

    Setup Entity Framework + DB Context

--- RunGroopWebApp/Models/AppUser.cs
+++ RunGroopWebApp/Models/AppUser.cs
@@ -1,14 +1,18 @@
-﻿namespace RunGroopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.Models
 {
     public class AppUser
     {
+        [Key]
+        public string Id { get; set; } 
         public int? Pace { get; set; }
 
         public int? Milage { get; set; }
 
         public Address? Address { get; set; }
 
-        public ICollection<Club> Clubs { get; set; }
-        public ICollection<Race> Races { get; set; }   
+        public ICollection<Club>? Clubs { get; set; }
+        public ICollection<Race>? Races { get; set; }   
     }
 }
