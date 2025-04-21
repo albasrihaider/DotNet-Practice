bb5d7718a5631b5274079c4667c46c0bc9fde648
Haider Albasri <haiderf3@gmail.com>
2025-04-21 04:52:22 +0300

    Race controller bug fix

--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -2,19 +2,21 @@
 using RunGroopWebApp.Data;
 using RunGroopWebApp.Models;
 
+
 namespace RunGroopWebApp.Controllers
 {
     public class RaceController : Controller
     {
         private readonly ApplicationDbContext _context;
 
+        [ActivatorUtilitiesConstructor]
         public RaceController(ApplicationDbContext context)
         {
             _context = context;
         }
         public IActionResult Index()
         {
-            List <Race> races= _context.Races.ToList();
+            List<Race> races= _context.Races.ToList();
             return View(races);
         }
     }
