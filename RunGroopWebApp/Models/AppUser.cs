23ca00e66a484cc20fcc164062efe09624a68f06
Haider Albasri <haiderf3@gmail.com>
2025-04-26 02:50:04 +0300

    Setup Identity framwork

--- RunGroopWebApp/Models/AppUser.cs
+++ RunGroopWebApp/Models/AppUser.cs
@@ -1,15 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace RunGroopWebApp.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        [Key]
-        public string Id { get; set; } 
+
         public int? Pace { get; set; }
 
         public int? Milage { get; set; }
-
+       
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
         public Address? Address { get; set; }
 
         public ICollection<Club>? Clubs { get; set; }
--- RunGroopWebApp/Program.cs
+++ RunGroopWebApp/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
 using RunGroopWebApp.Helpers;
 using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
 using RunGroopWebApp.Repository;
 using RunGroopWebApp.Services;
 
@@ -19,13 +22,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+   .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+   builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+
+
 
 var app = builder.Build();
 
 if(args.Length == 1 && args[0].ToLower() == "seeddata")
 {
-  //  Seed.SeedUserAndRolesAsync(app);
-    Seed.SeedData(app);
+ await  Seed.SeedUsersAndRolesAsync(app);
+    //Seed.SeedData(app);
 }
 
 // Configure the HTTP request pipeline.
