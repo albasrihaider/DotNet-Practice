488a6d15c141bb471283f2cc96accf6539d38427
Haider Albasri <haiderf3@gmail.com>
2025-04-27 09:17:47 +0300

    Setup Login functionality

---
+++ RunGroopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            // User is found, check password
+            if (!ModelState.IsValid) return View(loginViewModel);
+            var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+            if (user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user,loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    // Password is correct, sign in the user
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                // Password is incorrect
+                TempData["Error"] = "Invalid login attempt. Please try again.";
+                return View(loginViewModel);
+            }
+            // User not found
+            TempData["Error"] = "Invalid login attempt. Please try again.";
+            return View(loginViewModel);
+
+        }
+    }
+}
---
+++ RunGroopWebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
