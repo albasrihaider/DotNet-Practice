2e1833369afa24933531ae6670f60345a76fe2d6
Haider Albasri <haiderf3@gmail.com>
2025-04-27 19:00:15 +0300

    Added Register Functionality

---
+++ RunGroopWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Email Addres")]
+        [Required(ErrorMessage = "Email is required")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
