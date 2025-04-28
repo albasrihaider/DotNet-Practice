f9a561fcca9f5fc4b828e8d5357f58fbbaa7e67b
Haider Albasri <haiderf3@gmail.com>
2025-04-28 11:20:14 +0300

    Setup Dashboard logic

--- RunGroopWebApp/Controllers/DashboardController.cs
+++ RunGroopWebApp/Controllers/DashboardController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.ViewModels;
 
 namespace RunGroopWebApp.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly IDashboardRepository _dashboardRepository;
+
+        public DashboardController(IDashboardRepository dashboardRepository)
         {
-            return View();
+            _dashboardRepository = dashboardRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userRaces = await _dashboardRepository.GetAllUserRaces();
+            var userClubs = await _dashboardRepository.GetAllUserClubs();
+            var dashboardViewModel = new DashboardViewModel()
+            {
+                Races = userRaces,
+                Clubs = userClubs
+            };
+            return View(dashboardViewModel);
         }
     }
 }
