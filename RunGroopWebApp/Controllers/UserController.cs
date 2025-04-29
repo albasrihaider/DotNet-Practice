161028b13d9db301956ad34bd87535088f91a3e1
Haider Albasri <haiderf3@gmail.com>
2025-04-29 21:44:01 +0300

    Setup Users View which contains a list of users that are online

---
+++ RunGroopWebApp/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.ViewModels;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserController(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor)
+        {
+            _userRepository = userRepository;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+            List<UserViewModel> result = new List<UserViewModel>();
+            foreach (var user in users)
+            {
+                var userViewModel = new UserViewModel
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Milage = user.Milage
+                };
+                result.Add(userViewModel);
+            }
+            return View(result);
+        }
+    }
+}
